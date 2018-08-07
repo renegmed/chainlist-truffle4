c1b9f76572475edb04e09468be5ffbd71db2ba76
rene <renegmed@yahoo.com>
2018-08-07 14:31:36 -0400

    first commit, deploy-start

---
+++ truffle.js
@@ -0,0 +1,17 @@
+module.exports = {
+     // See <http://truffleframework.com/docs/advanced/configuration>
+     // to customize your Truffle configuration!
+     networks: {
+          ganache: {
+               host: "127.0.0.1",
+               port: 7545,
+               network_id: "5792" // Match any network id
+          },
+          chainskills: {
+            host: "localhost",
+            port: 8545,
+            network_id: "4224",
+            gas: 4700000
+          }
+     }
+};
